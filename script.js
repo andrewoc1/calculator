7ef3ea259c0926e40f691388fe39a7770081a909
andrewoc1 <andrewoc1111@gmail.com>
2026-06-27 12:54:20 -0400

    Complete Calculator Design and begin work on javascript functionality

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+//operator variables
+let firstNum;
+let secondNum;
+let finalNum;
+let operator;
 
+//basic math functions
+function add(a, b) {
+  return a + b;
+}
+console.log(add(3, 4));
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function clear() {}
+/*
+//update screen when different functions are called
+function updateScreen(a, b, operator) {
+  const results = document.querySelector(".results");
+}
+*/
+function operate(a, b, op) {}
+
+function handleClick(e) {
+  if (e.target.dataset.number) {
+    console.log(e.target.dataset.number);
+  }
+  if (e.target.dataset.operator) {
+    console.log(e.target.dataset.operator);
+  }
+  if (e.target.dataset.action) {
+    console.log(e.target.dataset.action);
+  }
+}
+
+const calcButtons = document.querySelectorAll(".btn-calc");
+calcButtons.forEach((btn) => {
+  btn.addEventListener("click", handleClick);
+});
+
+const resultBtn = document.querySelector(".results");
+resultBtn.addEventListener("click", operate);
