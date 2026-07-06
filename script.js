62757316467cdfca52bdc6bdb390f913ca37e2ee
andrewoc1 <andrewoc1111@gmail.com>
2026-07-06 10:48:15 -0400

    Complete Calculator Implementation

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //operator variables
-let firstNum;
-let secondNum;
-let finalNum;
-let operator;
+let firstNum = "";
+let secondNum = "";
+let operator = "";
 
 //basic math functions
 function add(a, b) {
@@ -22,25 +21,112 @@ function divide(a, b) {
   return a / b;
 }
 
-function clear() {}
-/*
-//update screen when different functions are called
-function updateScreen(a, b, operator) {
-  const results = document.querySelector(".results");
+function modulus(a, b) {
+  return a % b;
 }
-*/
-function operate(a, b, op) {}
+
+//update calculator screen
+function updateScreen() {
+  const results = document.querySelector(".results");
+  if (secondNum !== "") {
+    results.textContent = firstNum + " " + operator + " " + secondNum;
+  } else if (operator !== "") {
+    results.textContent = firstNum + " " + operator;
+  } else if (firstNum !== "") {
+    results.textContent = firstNum;
+  } else {
+    results.textContent = "0";
+  }
+}
+
+function operate(a, b, op) {
+  const parsedFirst = parseFloat(a);
+  const parsedSecond = parseFloat(b);
+
+  switch (op) {
+    case "+":
+      return add(parsedFirst, parsedSecond);
+    case "-":
+      return substract(parsedFirst, parsedSecond);
+    case "*":
+      return multiply(parsedFirst, parsedSecond);
+    case "/":
+      return divide(parsedFirst, parsedSecond);
+
+    case "%":
+      return modulus(parsedFirst, parsedSecond);
+  }
+}
+
+function handleDecimals(num) {
+  const numStr = num.toString();
+  if (numStr.includes(".")) {
+    if (numStr.split(".")[1].length > 8) {
+      return Math.round(num * 10 ** 8) / 10 ** 8;
+    }
+  }
+  return num;
+}
 
 function handleClick(e) {
   if (e.target.dataset.number) {
-    console.log(e.target.dataset.number);
+    const clickedNumber = e.target.dataset.number;
+
+    if (!operator) {
+      if (clickedNumber === "." && firstNum.includes(".")) {
+        return;
+      }
+      firstNum += clickedNumber;
+    } else {
+      if (clickedNumber === "." && secondNum.includes(".")) {
+        return;
+      }
+
+      secondNum += clickedNumber;
+    }
   }
   if (e.target.dataset.operator) {
-    console.log(e.target.dataset.operator);
+    const clickedOperator = e.target.dataset.operator;
+    if (firstNum && secondNum && operator) {
+      const rawResult = operate(firstNum, secondNum, operator);
+      result = handleDecimals(rawResult);
+      firstNum = result.toString();
+      secondNum = "";
+      operator = clickedOperator;
+    } else if (firstNum) {
+      operator = clickedOperator;
+    }
   }
+
   if (e.target.dataset.action) {
-    console.log(e.target.dataset.action);
+    const action = e.target.dataset.action;
+
+    if (action === "equals") {
+      if (firstNum && secondNum && operator) {
+        const rawResult = operate(firstNum, secondNum, operator);
+        result = handleDecimals(rawResult);
+        firstNum = result.toString();
+        firstNum = result.toString();
+        secondNum = "";
+        operator = "";
+      }
+    }
+    if (action === "clear") {
+      firstNum = "";
+      secondNum = "";
+      operator = "";
+    }
+    if (action == "delete") {
+      if (firstNum && secondNum && operator) {
+        secondNum = secondNum.slice(0, -1);
+      } else if (firstNum && operator) {
+        operator = "";
+      } else if (firstNum) {
+        firstNum = firstNum.slice(0, -1);
+      }
+    }
   }
+  updateScreen();
 }
 
 const calcButtons = document.querySelectorAll(".btn-calc");
